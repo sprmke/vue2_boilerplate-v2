4ca7097e1273f1ddfe29461bb547054389f808d1
Michael Manlulu <michael.manlulu@tooltwist.com>
2019-10-21 20:57:52 +0800

    updated overview docs

--- src/app/modules/docs-view/components/getting-started/documentation-overview/documentation-overview.vue
+++ src/app/modules/docs-view/components/getting-started/documentation-overview/documentation-overview.vue
@@ -2,33 +2,49 @@
   <div class="documentation-overview">
     <p>A project documentation that helps the developers to be familiarized to every features this boilerplate brings and list of coding standards for a cleaner, uniform and more maintable code.</p>
     <hr>
-    <h4>Table of contents</h4>
-    <p>Please check every modules to be more familiarized to the features, processes and coding practices of this application.</p>
-    <ul>
-      <li><router-link to="/docs/technologies">Technologies</router-link></li>
-      <li><router-link to="">Firebase Auth &amp; Database</router-link></li>
-      <li>Project Structure
-        <ul>
-          <li><router-link to="">Main</router-link></li>
-          <li><router-link to="">Modules</router-link></li>
-          <li><router-link to="">Styles</router-link></li>
-        </ul>
-      </li>
-      <li>Coding Standards
-        <ul>
-          <li>
-            <router-link to="">Root</router-link></li>
-          <li>
-            <router-link to="">App</router-link></li>
-          <li>
-            <router-link to="">Modules</router-link></li>
-          <li>
-            <router-link to="">Template Generator</router-link></li>
-          <li>
-            <router-link to="">Styles</router-link></li>
-        </ul> 
-      </li>
-    </ul>
+    <!-- Table of contents -->
+    <section class=mb-5>
+      <h4>Table of contents</h4>
+      <p>Please check every modules to be more familiarized to the features, processes and coding practices of this application.</p>
+      <ul>
+        <li><router-link to="/docs/technologies">Technologies</router-link></li>
+        <li><router-link to="">Firebase Auth &amp; Database</router-link></li>
+        <li>Project Structure
+          <ul>
+            <li>
+              <router-link to="/docs/root">Root</router-link></li>
+            <li>
+              <router-link to="/docs/app">App</router-link></li>
+            <li>
+              <router-link to="/docs/modules">Modules</router-link></li>
+            <li>
+              <router-link to="/docs/generator">Template Generator</router-link></li>
+            <li>
+              <router-link to="/docs/styles">Styles</router-link></li>
+          </ul> 
+        </li>
+        <li>Coding Standards
+          <ul>
+            <li>
+              <router-link to="/docs/template">Template</router-link></li>
+            <li>
+              <router-link to="/docs/css">CSS and Styles</router-link></li>
+            <li>
+              <router-link to="/docs/vue">Vue</router-link></li>
+            <li>
+              <router-link to="/docs/javascript">JavaScript</router-link></li>
+            <li>
+              <router-link to="/docs/unit-testing">Unit Testing</router-link></li>
+          </ul> 
+        </li>
+      </ul>
+    </section>
+    <!-- Installation -->
+    <section class="mb-5">
+      <h4>Installation</h4>
+      <p>Please check every modules to be more familiarized to the features, processes and coding practices of this application.</p>
+      <img style="width: 60%" class="img-fluid" src="@/assets/docs/gettings-started/installation.png" alt="Installation">
+    </section>
   </div>
 </template>
 
