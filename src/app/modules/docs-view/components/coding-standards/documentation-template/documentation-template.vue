849de3a23123519f86352ed01673ce71b33474c2
Michael Manlulu <michael.manlulu@tooltwist.com>
2019-10-25 16:59:56 +0800

    added docs template

--- src/app/modules/docs-view/components/coding-standards/documentation-template/documentation-template.vue
+++ src/app/modules/docs-view/components/coding-standards/documentation-template/documentation-template.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="documentation-template">
-    
+    <p>This page contains the list of the best coding practices on creating HTML layout and working with our templates.</p>
+    <hr>
+    <section>
+      <h4>Essential</h4>
+      <ul>
+        <li>Make sure the component root element has a class of the component name.</li>
+        <li>Use correct HTML tags base on its need.</li>
+        <li>Prevent declaring unnecessary <strong>div</strong> elements.</li>
+        <li>Declare unique ids and classes whenever possible.</li>
+        
+      </ul>
+      <h4>Recommended</h4>
+        <ul>
+          <li>Make your webpage SEO friendly.</li>
+          <li>Use Bootstrap4 and custom css utility classes for commonly used styles instead of creating new class and custom styles.</li>
+          <li>Try to use HTML5 new tags and never use deprecated ones.</li>
+          <li>A good HTML structure can save you up to 20% less css and stress.</li>
+          <li>Creating HTML layout structure from the scratch is more recommended than copy pasting html layouts.</li>
+        </ul>
+    </section>
   </div>
 </template>
 
